a7ca11d30e98a40e9f279e8e50a6b7f3b2215523
ogwebdev2 <pucmario@gmail.com>
2025-03-21 15:13:23 -0600

    feat: implement auth with access token

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,5 +1,17 @@
 const VerifyToken = (req, res, next) => {
-    res.send('Token verified')
+    try {
+        const access_token = process.env.ACCESS_TOKEN
+        const token = req.query["hub.verify_token"]
+        const challenge = req.body["hub.challenge"]
+
+        if(challenge !== null && token === access_token) {
+            res.send(challenge)
+        } else {
+            res.status(401).send('Unauthorized')
+        }
+    } catch (error) {
+        res.status(400).send('Invalid token')
+    }
 }
 
 const ReceivedMessage = (req, res) => {
