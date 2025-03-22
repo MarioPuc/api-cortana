53cce4939bf3b4e2c874a55e7096b48269e4e7d0
ogwebdev2 <pucmario@gmail.com>
2025-03-22 12:12:04 -0600

    fix: assignment on constant change to let

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -35,7 +35,7 @@ const ReceivedMessage = (req, res) => {
 }
 
 const GetTextUser = (messages) => {
-    const text = ""
+    let text = ""
     const typeMessage = messages.type
     if(typeMessage === 'text') {
         text = messages.text.body
