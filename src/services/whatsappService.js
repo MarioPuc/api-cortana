86c6315dfee24407999a2ab3f4bb0e402918cccd
ogwebdev2 <pucmario@gmail.com>
2025-03-22 15:25:39 -0600

    fix: fix whatsapp service headers

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -20,18 +20,17 @@ function SendMessageWhatsApp(textResponse, phone) {
         body: data,
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer EAAJQx4IZByKgBO7p3pInc5UCNQMulgZBpfQAFzmJ4lK3n1N1FIg6XVRqOtIxZCdckChB2zNRO8mjCsecmo64YbI8eoQe4iHqp1TFRw7u8ZBFury9aGQOwBKvRgf4uMaIVwZCL3pWTf4AD5nDB4zzXVyBLAMZCEdWmIvMdj9U0fxAnvTSpAPMFSwrJNdWz5a3bXCkbMVAJNwh5l1vNKM2II267MhGIZD'
+            Authorization: 'Bearer EAAJQx4IZByKgBO7p3pInc5UCNQMulgZBpfQAFzmJ4lK3n1N1FIg6XVRqOtIxZCdckChB2zNRO8mjCsecmo64YbI8eoQe4iHqp1TFRw7u8ZBFury9aGQOwBKvRgf4uMaIVwZCL3pWTf4AD5nDB4zzXVyBLAMZCEdWmIvMdj9U0fxAnvTSpAPMFSwrJNdWz5a3bXCkbMVAJNwh5l1vNKM2II267MhGIZD'
         }
     }
 
     const req = https.request(options, (res) => {
-        let body = ''
-        res.on('data', (chunk) => {
+        res.on("data", (chunk) => {
             process.stdout.write(chunk)
         })
     })
 
-    req.on('error', (error) => {
+    req.on("error", (error) => {
         console.error(error)
     })
 
