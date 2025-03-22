7fcc37b5f05295f93341561362dd2fe55e21c6d7
ogwebdev2 <pucmario@gmail.com>
2025-03-22 16:06:51 -0600

    fix: import

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -1,4 +1,5 @@
 const https = require('https')
+const fs = require('fs')
 const myConsole = new console.Console(fs.createWriteStream('./logs.txt'))
 
 function SendMessageWhatsApp(textResponse, phone) {
